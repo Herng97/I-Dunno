fe09f03861497a9103dd8723c4db677ba18cbafa
Herng97 <sbherng1997@live.com>
2016-12-03 16:32:55 +0800

    up

---
+++ Assets/List_Item_Coin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class List_Item_Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
