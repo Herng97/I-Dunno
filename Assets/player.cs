4d58b50a8ddd1dddab4fd9262266badf876a0e8c
42 <hadean92@gmail.com>
2016-12-04 19:23:44 +0800

    up

--- Assets/player.cs
+++ Assets/player.cs
@@ -10,7 +10,8 @@ public class player : MonoBehaviour
     public KBEngine.Avatar entity;
     public GpsSystem GPS;
     Vector3 StartPoint;// = new Vector3(120.6499191f,24.1791539f,1);
-
+    public Animator Anim;
+    public Vector3 TargetPosition;
     // Use this for initialization
     void Start()
     {
@@ -31,14 +32,32 @@ public class player : MonoBehaviour
             {
                 if (StartPoint == Vector3.zero)
                     StartPoint = new Vector3(120.6465792f, 24.1790243f, 1);
-                if (Vector3.Distance(iso.Position, (GPS.GetVector3() - StartPoint) * 20000) > 5)
-                    iso.Position = (GPS.GetVector3() - StartPoint) * 20000;
-                else
-                    iso.Position = Vector3.MoveTowards(iso.Position, (GPS.GetVector3() - StartPoint) * 20000, 1 * Time.deltaTime);
 
+                TargetPosition = (GPS.GetVector3() - StartPoint) * 20000;
+                if (Vector3.Distance(iso.Position, TargetPosition) > 5)
+                    iso.Position = TargetPosition;
+                else
+                {
+
+                    if (Vector3.Distance(iso.Position, TargetPosition) < 0.5f)
+                    {
+                        Anim.SetBool("isWalk", false);
+                    }
+                    else
+                    {
+                        Vector3 target = Vector3.MoveTowards(iso.Position, TargetPosition, Time.deltaTime);
+                        Anim.SetBool("isWalk", true);
+                        Anim.SetFloat("x", target.x - iso.Position.x);
+                        Anim.SetFloat("y", target.y - iso.Position.y);
+
+                        iso.Position = target;
+
+                    }
+                }
+                KBEngine.Event.fireIn("UpdatePlayer", iso.Position);
             }
-            KBEngine.Event.fireIn("UpdatePlayer", iso.Position);
-    
+
+
         }
 
     }
